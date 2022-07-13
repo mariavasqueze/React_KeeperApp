4b7e2db1d2660a6067ba95f1d4ceae3e8e3e084d
Maria Vasquez <mariadelmarvze@gmail.com>
2022-07-12 17:12:53 -0700

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const date = new Date();
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright © {date.getFullYear()}</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div>
+      <h1></h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default Note;
